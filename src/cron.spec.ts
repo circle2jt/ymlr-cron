33e2986b440b6ea565a08fa439daa68427fdbea3
circle 2π <circle2jt@gmail.com>
2023-10-13 10:12:38 +0700

    feat: add cron tag

---
+++ src/cron.spec.ts
@@ -0,0 +1,34 @@
+import { type ElementProxy } from 'ymlr/src/components/element-proxy'
+import { Testing } from 'ymlr/src/testing'
+import { Cron } from './cron'
+
+let cron: ElementProxy<Cron>
+
+beforeEach(async () => {
+  await Testing.reset()
+})
+
+afterEach(async () => {
+  await cron.dispose()
+})
+
+test('Test simple cron each 1 sec', async () => {
+  Testing.vars.ok = false
+  cron = await Testing.createElementProxy(Cron, {
+    time: '* * * * * *',
+    scheduled: true,
+    runs: [
+      {
+        vars: {
+          ok: true,
+          lastDate: '${ $parentState.lastDate }'
+        }
+      }, {
+        stop: null
+      }
+    ]
+  })
+  await cron.exec()
+  expect(Testing.vars.ok).toBe(true)
+  expect(Testing.vars.lastDate).toBeInstanceOf(Date)
+})
